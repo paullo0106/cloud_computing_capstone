bc9e62ff181195b06c1bcf1bdc52b4abed1a05f4
Paul Lo <elohim123@gmail.com>
2016-02-18 01:06:57 +0800

    Add comment for loglog plot

--- popularity_plot.py
+++ popularity_plot.py
@@ -8,9 +8,23 @@ y = list()
 rerank = 1
 pre_rank = 1
 pre_frequency = None
+'''
+popularity_rank file example:
+1  5000
+2  4840
+3  4820
+4  4315
+5  4315
+6  3788
+7  3715
+8  3396
+...
+...
+'''
 with open('popularity_rank', 'rb') as INPUT:
     for line in INPUT.readlines():
         rank, frequency = line.split()
+        # correct the same frequency might have different ranks
         if frequency == pre_frequency:
             rerank = pre_rank
         else:
@@ -23,8 +37,8 @@ with open('popularity_rank', 'rb') as INPUT:
         pre_rank = rerank
 
 
-plt.loglog(x, y, basex=10, basey=10)
+plt.loglog(x, y, basex=10, basey=10)  # log-log plot
 plt.title('Popularity distribution')
-#plt.plot(x, y)
+#plt.plot(x, y)  # normal x-y plot
 plt.show()
 
